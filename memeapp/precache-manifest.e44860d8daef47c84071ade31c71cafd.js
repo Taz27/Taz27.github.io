b6a1770b9749392cf45e1caf562767e1569c678c
Taran Mand <tsmand@yahoo.com>
2020-01-13 19:28:56 +1100

    Added Todo App and Meme App

---
+++ memeapp/precache-manifest.e44860d8daef47c84071ade31c71cafd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302314150cb175500ac990e3fe6e6259",
+    "url": "/memeapp/index.html"
+  },
+  {
+    "revision": "079cef76d535b5676579",
+    "url": "/memeapp/static/css/main.76ecced6.chunk.css"
+  },
+  {
+    "revision": "56e0ccb6926fe8f178ba",
+    "url": "/memeapp/static/js/2.3400416c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memeapp/static/js/2.3400416c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "079cef76d535b5676579",
+    "url": "/memeapp/static/js/main.026dc007.chunk.js"
+  },
+  {
+    "revision": "6771a501b5b1370217cf",
+    "url": "/memeapp/static/js/runtime-main.7e950a58.js"
+  }
+]);
---
+++ todoapp/precache-manifest.c087022157ec7c93ee7c26d1f64f519e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e55703ae4d6439ac13aca744efb6961",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "2c2575501c14100c7bff",
+    "url": "/todoapp/static/css/main.15d499b7.chunk.css"
+  },
+  {
+    "revision": "e2c3738ff867b9cf550e",
+    "url": "/todoapp/static/js/2.29fe44ad.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/todoapp/static/js/2.29fe44ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c2575501c14100c7bff",
+    "url": "/todoapp/static/js/main.fdaa538b.chunk.js"
+  },
+  {
+    "revision": "46230ecf1c7ed517a45e",
+    "url": "/todoapp/static/js/runtime-main.5091b78b.js"
+  }
+]);
