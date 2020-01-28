28acd7a9b789e7a6052ad16298b233ad3664fca1
Taran Mand <tsmand@yahoo.com>
2020-01-28 18:03:17 +1100

    Add Speed Typing React Game

---
+++ speedtypinggame/precache-manifest.6dd5def565e073b861ac9a0a159685b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c9d493ee945cde539496db5fdab443",
+    "url": "/speedtypinggame/index.html"
+  },
+  {
+    "revision": "1e298503135f96796c8e",
+    "url": "/speedtypinggame/static/css/main.c3adac7b.chunk.css"
+  },
+  {
+    "revision": "4b678344c84d93390cd8",
+    "url": "/speedtypinggame/static/js/2.cb5898b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/speedtypinggame/static/js/2.cb5898b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e298503135f96796c8e",
+    "url": "/speedtypinggame/static/js/main.2b1727a8.chunk.js"
+  },
+  {
+    "revision": "0f869f3aa552e989b3c4",
+    "url": "/speedtypinggame/static/js/runtime-main.02bae9a7.js"
+  }
+]);
