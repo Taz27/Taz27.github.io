639b0df53b71cd6fdcedb7dcd7a94a35b1295035
Taran Mand <tsmand@yahoo.com>
2020-02-01 19:37:50 +1100

    add picsome app

---
+++ picsomeapp/precache-manifest.d9275e29a3290af62b570b61ed74d20f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833750ee6ffa9f8a409cc9ce106da2a7",
+    "url": "/picsomeapp/index.html"
+  },
+  {
+    "revision": "cc10e57d1b5353aa5c9a",
+    "url": "/picsomeapp/static/css/main.2d24cbed.chunk.css"
+  },
+  {
+    "revision": "e5f373a033e91548b03f",
+    "url": "/picsomeapp/static/js/2.6d03c205.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/picsomeapp/static/js/2.6d03c205.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc10e57d1b5353aa5c9a",
+    "url": "/picsomeapp/static/js/main.935b30e3.chunk.js"
+  },
+  {
+    "revision": "a03822bbc08bed4e71e7",
+    "url": "/picsomeapp/static/js/runtime-main.18ed45b0.js"
+  }
+]);
