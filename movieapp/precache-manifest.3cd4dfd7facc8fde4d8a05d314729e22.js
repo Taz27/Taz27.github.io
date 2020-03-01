000d3f3a8318942fdd3af73e49d89e3ae7c652b8
Taran Mand <tsmand@yahoo.com>
2020-03-01 19:30:00 +1100

    Add Movie Search App

---
+++ movieapp/precache-manifest.3cd4dfd7facc8fde4d8a05d314729e22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c3b673b328efcde9090103d0757a72",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "38054c62ce0360bf4e77",
+    "url": "/movieapp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "67e515e0f750c146f332",
+    "url": "/movieapp/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "38054c62ce0360bf4e77",
+    "url": "/movieapp/static/js/2.9bdab08c.chunk.js"
+  },
+  {
+    "revision": "66adf9e2ca280da1c5b99ce51bef8991",
+    "url": "/movieapp/static/js/2.9bdab08c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67e515e0f750c146f332",
+    "url": "/movieapp/static/js/main.11bb91fb.chunk.js"
+  },
+  {
+    "revision": "0aa5b73ad6a261c26efc",
+    "url": "/movieapp/static/js/runtime-main.2e516a57.js"
+  }
+]);
