e1111b922893d41b1b8b6c4027aae6d8cd59500d
Taran Mand <tsmand@yahoo.com>
2020-10-17 14:50:57 +1100

    add pokemon graphql app

---
+++ pokemon-graphql-app/precache-manifest.129d799a7c74ea4b1e54a581eb7fcb0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ee0174bcafd494844a361fa36d376f",
+    "url": "/pokemon-graphql-app/index.html"
+  },
+  {
+    "revision": "9caa805679615ad2ce42",
+    "url": "/pokemon-graphql-app/static/css/main.2c9e1d12.chunk.css"
+  },
+  {
+    "revision": "79629e489408258a8603",
+    "url": "/pokemon-graphql-app/static/js/2.21ae3a1d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/pokemon-graphql-app/static/js/2.21ae3a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9caa805679615ad2ce42",
+    "url": "/pokemon-graphql-app/static/js/main.e75a852e.chunk.js"
+  },
+  {
+    "revision": "9fd262768cd2c6b4d8ef",
+    "url": "/pokemon-graphql-app/static/js/runtime-main.4634fbfd.js"
+  }
+]);
